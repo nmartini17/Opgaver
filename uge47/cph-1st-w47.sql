0335ae6431cc2af4ac3b72118301294329622131
Nicolai <cph-nm172@cphbusiness.dk>
2020-11-19 14:33:53 +0100

    Havde ikke gemt SQL filen
    Så her er den som jeg rent faktisk lavede...

--- uge47/cph-1st-w47.sql
+++ uge47/cph-1st-w47.sql
@@ -4,6 +4,47 @@ SELECT * FROM actor;
 
 SELECT * FROM actor WHERE last_name = "CAGE";
 
-SELECT * FROM actor WHERE first_name != "ZERO" OR "NICK";
+SELECT * FROM actor WHERE first_name NOT IN  ("ZERO", "NICK");
 
-SELECT * FROM actor WHERE first_name = "NICK" OR "JOHNNY" OR "JAMES" OR "MORGAN" OR "WHOOPI";
+SELECT * FROM actor WHERE first_name IN ('NICK', 'JOHNNY', 'JAMES', 'MORGAN', 'WHOOPI');
+
+SELECT * FROM actor WHERE actor_id BETWEEN 50 AND 150;
+
+SELECT * FROM actor WHERE first_name LIKE 'C%';
+
+SELECT * FROM actor ORDER BY first_name;
+
+SELECT * FROM actor ORDER BY last_name DESC;
+
+SELECT COUNT(*) FROM actor;
+
+SELECT COUNT(DISTINCT first_name) FROM actor;
+/*
+SELECT * from film_category WHERE category_id IN(11);
+
+SELECT * FROM film_category;
+*/
+
+
+INSERT INTO actor(first_name,last_name) VALUES ("Nicolai","Martini");
+SELECT * FROM actor;
+
+INSERT INTO film_actor(actor_id,film_id) VALUES (201,7);
+INSERT INTO film_actor(actor_id,film_id) VALUES (201,2);
+INSERT INTO film_actor(actor_id,film_id) VALUES (201,3);
+INSERT INTO film_actor(actor_id,film_id) VALUES (201,4);
+INSERT INTO film_actor(actor_id,film_id) VALUES (201,5);
+SELECT * FROM film_actor WHERE actor_id LIKE 201;
+
+SELECT * FROM film_actor WHERE actor_id = 201;
+UPDATE actor SET first_name = "Anders", last_name = "And" WHERE actor_id = 201;
+
+DELETE FROM film_actor WHERE actor_id = 201;
+DELETE FROM actor WHERE actor_id = 201;
+
+
+SELECT * FROM category;
+
+SELECT title FROM film
+JOIN film_category ON film.title = film_category.film_id;
+
